5e10ac31c49d8348dfe9d5f3ae7eb300b6e0c34d
krishnasutar7 <krishnasutar.xworkz@gamail.com>
2022-11-25 20:13:40 +0530

    variable related files

---
+++ SuperMarket.java
@@ -0,0 +1,70 @@
+class SuperMarket{
+static String fruit[]={"Apple","Orange","Kiwi","Warermelon","Straberry","Pappay","Lemon","Plum","Grape"};
+static String vegitables[]={"Potato","Tomato","Cabbage","Brinjal","Cucumber","Carrot","Peas","Radish","Mushroom","Pumpkin"};
+static String chocolates[]= {"kiss me","Kit Kat","Dairy Milk","Perk","5Star","milkybar","Perk","Snackers","mars","Galaxy"};
+static String perfumes[]= {"Denver","Vilan","Bently","Skin","Guess","Nike","Parkavenue","Chenal","Ventage","Savage"};
+static String clothes[]= {"Shirt","Sweater","T-Shirt","Coat","Suit","jacket","Swimsuit","Shorts"};
+static String dryfruits[]={"Almond","Apricot","Cashew Nut","Dates","Coconut","Flax","Groundnut","Pistachio","Kismis","Walnut"};
+
+public static void main(String a[]){
+	
+	System.out.println("The list of SuperMarket AVAILABLE FRUITS ARE");
+	System.out.println(fruit.length);
+	
+	for(int i=0; i<fruit.length; i++){
+	
+	System.out.println(fruit[i]);
+	System.out.println("                                           ");
+	}
+	
+	System.out.println("The list of SuperMarket AVAILABLE VEGITABLES ARE");
+	System.out.println(vegitables.length);
+	for(int j=0; j<vegitables.length; j++){
+	
+	System.out.println(vegitables[j]);}
+	System.out.println("                                           ");
+	
+	System.out.println("The list of SuperMarket AVAILABLE CHOCOLATE ARE");
+	System.out.println(chocolates.length);
+	for(int k=0; k<chocolates.length; k++){
+	
+	System.out.println(chocolates[k]);}
+	System.out.println("                                           ");
+	
+	System.out.println("The list of SuperMarket AVAILABLE PERFUME ARE");
+	System.out.println(perfumes.length);
+	for(int l=0; l<perfumes.length; l++){
+
+	System.out.println(fruit[l]);}
+	System.out.println("                                           ");
+	
+	System.out.println("The list of SuperMarket AVAILABLE CLOTHS ARE");
+	System.out.println(clothes.length);
+	for(int m=0; m<clothes.length; m++){
+
+	System.out.println(clothes[m]);}
+	System.out.println("                                           ");
+	
+	System.out.println("The list of SuperMarket AVAILABLE DRYFRUITS ARE");
+	System.out.println(dryfruits.length);
+	for(int n=0; n<dryfruits.length; n++){
+
+	System.out.println(dryfruits[n]);
+	System.out.println("                                           ");
+	}
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+		
+	
+	
+}
