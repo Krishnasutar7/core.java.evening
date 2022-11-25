63e96e1789404ea430d95a01eacbbf96732a42ff
krishnasutar7 <krishnasutar.xworkz@gamail.com>
2022-11-25 20:28:38 +0530

    commiting methode files

---
+++ SwiggyTester.java
@@ -0,0 +1,15 @@
+class SwiggyTester{
+
+  public static void main(String swiggy[]){
+                     // class name .methode 
+  double priceOfItem = Swiggy.findItem("daalkichadi");
+  
+  
+  System.out.println("The item price is "+  priceOfItem);
+  
+  
+  
+  }
+
+
+}
---
+++ Zomato.java
@@ -0,0 +1,20 @@
+class Zomato{
+
+  public static double findItem(String item){
+  System.out.println("Invoked findItem()");
+  if(item=="Biryani"){
+  return 225.00;
+  }
+  else{
+     System.out.println("Item not found");
+	 
+  }
+  // if  9
+  System.out.println("Invoked findItem");
+  
+  return 0.0;
+  
+  
+  }
+
+}
