d6bb0d339aa208d1696cf9d298f86d273e5c0a01
krishnasutar7 <krishnasutar.xworkz@gamail.com>
2022-11-26 11:43:29 +0530

    commiting variable related file

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+class Welcome{
+public static void main(String[] a){
+	String filename = "krishna";
+	String lastname = "sutar";
+	
+System.out.print(filename);
+System.out.print("   "+ lastname);
+
+}
+
+}
